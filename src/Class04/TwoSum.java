7fdd98e6d3859fa51eac083e60e0f63964404378
bittoo aggarwal <bitz1119@gmail.com>
2021-04-23 19:04:15 +0530

    classes

---
+++ src/Class04/TwoSum.java
@@ -0,0 +1,34 @@
+package Class04;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,4,5,8,10,15,19,21};
+        int sum = 18;
+        int start = 0;
+        int end = arr.length-1;
+        boolean isAnsFound = false;
+        while (start < end){
+            if(sum < arr[start] + arr[end] ){
+                end--;
+            }
+            else if(sum > arr[start] + arr[end]){
+                start++;
+            }
+            else{
+                isAnsFound = true;
+                break;
+            }
+        }
+
+        if (isAnsFound == true){
+            System.out.println("Ans is found");
+        }
+        else{
+            System.out.println("Not found");
+        }
+
+    }
+
+}
---
+++ src/Class06/Kadane.java
@@ -0,0 +1,27 @@
+package Class06;
+
+public class Kadane {
+
+    public static void main(String[] args) {
+        int[] arr = {-1,-2,-3,-4,-5};
+
+        int localSum = 0;
+        int globalSum = 0;
+        for (int i = 0;i<arr.length ; i++){
+            localSum = localSum + arr[i];
+            if(globalSum < localSum){
+                globalSum = localSum;
+            }
+            // whether to take localsum forward or not
+            if(localSum < 0){
+                localSum = 0;
+            }
+        }
+        // if all the numbers are nagetive
+        if(globalSum == 0){
+            globalSum = findmin(arr);
+        }
+        System.out.println(globalSum);
+    }
+
+}
