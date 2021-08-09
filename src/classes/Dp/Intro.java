fc5c58e2c9b0f71ae89988924c2ed2473163e7bb
Aman Kumar <amanku@microsoft.com>
2021-08-10 00:19:49 +0530

    dp-6

--- src/classes/Dp/Intro.java
+++ src/classes/Dp/Intro.java
@@ -33,7 +33,12 @@ public class Intro {
         // String str2 = "AEDFHR";
         // System.out.println(LCommonSubs(str1, str2));
         // System.out.println(LongestPalindromicSubstring("abccbd"));
-        System.out.println(MinCutPalindrome("abccbd"));
+        // System.out.println(MinCutPalindrome("abccbd"));
+        int[] wts = { 2, 5, 1, 3, 4 };
+        int[] prices = { 15, 14, 10, 45, 30 };
+        int[][] strg = new int[wts.length+1][8];
+        System.out.println(KnapSack(7, wts, prices, wts.length,strg));
+        System.out.println(KnapSackTabulation(7,wts,wts.length,prices));
     }
 
     public static int fib(int n) {
@@ -131,6 +136,8 @@ public class Intro {
 
         return strg[n];
     }
+
+
 
     public static int mazePaths(int dr, int dc) {
         int[][] strg = new int[dr + 1][dc + 1];
@@ -272,87 +279,86 @@ public class Intro {
             }
         }
 
-        return strg[strg.length-1];
+        return strg[strg.length - 1];
     }
 
     public static int CoinChangeComb(int[] arr, int target) {
         int[] strg = new int[target + 1];
         strg[0] = 1;
         for (int i = 0; i < arr.length; i++) {
-                for (int j = 1; j < strg.length; j++) {
+            for (int j = 1; j < strg.length; j++) {
                 if (j - arr[i] >= 0) {
                     strg[j] += strg[j - arr[i]];
                 }
             }
         }
 
-        return strg[strg.length-1];
+        return strg[strg.length - 1];
     }
 
-    public static int MaxSumNoAdjacent(int[] arr){
+    public static int MaxSumNoAdjacent(int[] arr) {
         int[][] strg = new int[2][arr.length];
         strg[0][0] = arr[0];
         strg[1][0] = 0;
-        for(int i=1;i<arr.length;i++){
-            strg[0][i] = strg[1][i-1]+arr[i];
-            strg[1][i] = Math.max(strg[0][i-1],strg[1][i-1]);
+        for (int i = 1; i < arr.length; i++) {
+            strg[0][i] = strg[1][i - 1] + arr[i];
+            strg[1][i] = Math.max(strg[0][i - 1], strg[1][i - 1]);
         }
 
-        return Math.max(strg[0][arr.length-1],strg[1][arr.length-1]);
+        return Math.max(strg[0][arr.length - 1], strg[1][arr.length - 1]);
     }
 
-    public static int MaxSumNoAdjacentBtr(int[] arr){
+    public static int MaxSumNoAdjacentBtr(int[] arr) {
         int exclude = 0;
         int include = arr[0];
-        for(int i=1;i<arr.length;i++){
+        for (int i = 1; i < arr.length; i++) {
             int temp = include;
-            include = exclude +arr[i];
-            exclude = Math.max(exclude,temp);
+            include = exclude + arr[i];
+            exclude = Math.max(exclude, temp);
         }
 
-        return Math.max(exclude,include);
+        return Math.max(exclude, include);
     }
 
-    public static int LCommonSubs(String first, String sec){
-        int[][] strg = new int[first.length()+1][sec.length()+1];
-        for(int i=0;i<strg.length;i++){
-            for(int j=0;j<strg[0].length;j++){
-                if(i==0){
-                    strg[i][j]=0;
-                }else if(j==0){
+    public static int LCommonSubs(String first, String sec) {
+        int[][] strg = new int[first.length() + 1][sec.length() + 1];
+        for (int i = 0; i < strg.length; i++) {
+            for (int j = 0; j < strg[0].length; j++) {
+                if (i == 0) {
                     strg[i][j] = 0;
-                }else{
-                    if(first.charAt(i-1)==sec.charAt(j-1)){
-                        strg[i][j] = strg[i-1][j-1]+1;
-                    }else{
-                        strg[i][j] = Math.max(strg[i-1][j],strg[i][j-1]);
+                } else if (j == 0) {
+                    strg[i][j] = 0;
+                } else {
+                    if (first.charAt(i - 1) == sec.charAt(j - 1)) {
+                        strg[i][j] = strg[i - 1][j - 1] + 1;
+                    } else {
+                        strg[i][j] = Math.max(strg[i - 1][j], strg[i][j - 1]);
                     }
                 }
             }
         }
 
-        return strg[strg.length-1][strg[0].length-1];
+        return strg[strg.length - 1][strg[0].length - 1];
     }
 
-
-    public static int LongestPalindromicSubstring(String str){
+    public static int LongestPalindromicSubstring(String str) {
         boolean[][] strg = new boolean[str.length()][str.length()];
         int longPalinSubs = 0;
-        for(int gap =0;gap<str.length();gap++){
-            for(int i=0,j=gap;j<str.length();i++,j++){
-                if(gap==0){
+        for (int gap = 0; gap < str.length(); gap++) {
+            for (int i = 0, j = gap; j < str.length(); i++, j++) {
+                if (gap == 0) {
                     strg[i][j] = true;
-                    longPalinSubs =1;
-                }else if(gap==1){
-                    if(str.charAt(i)==str.charAt(j)){
+                    longPalinSubs = 1;
+                } else if (gap == 1) {
+                    if (str.charAt(i) == str.charAt(j)) {
                         strg[i][j] = true;
-                        longPalinSubs=2;
+                        longPalinSubs = 2;
                     }
-                }else{
-                    if(str.charAt(i)==str.charAt(j)){
-                        if(strg[i+1][j-1]==true){
+                } else {
+                    if (str.charAt(i) == str.charAt(j)) {
+                        if (strg[i + 1][j - 1] == true) {
                             strg[i][j] = true;
-                            longPalinSubs = gap+1;
+                            longPalinSubs = gap + 1;
                         }
                     }
                 }
@@ -362,25 +368,25 @@ public class Intro {
         return longPalinSubs;
     }
 
-    public static int MinCutPalindrome(String str){
+    public static int MinCutPalindrome(String str) {
         int[][] strg = new int[str.length()][str.length()];
-        for(int gap =0;gap<str.length();gap++){
-            for(int i=0,j=gap;j<str.length();i++,j++){
-                if(gap==0){
+        for (int gap = 0; gap < str.length(); gap++) {
+            for (int i = 0, j = gap; j < str.length(); i++, j++) {
+                if (gap == 0) {
                     strg[i][j] = 0;
-                }else if(gap==1){
-                    if(str.charAt(i)==str.charAt(j)){
+                } else if (gap == 1) {
+                    if (str.charAt(i) == str.charAt(j)) {
                         strg[i][j] = 0;
-                    }else{
-                        strg[i][j]=1;
+                    } else {
+                        strg[i][j] = 1;
                     }
-                }else{
-                    if(str.charAt(i)==str.charAt(j)&&strg[i+1][j-1]==0){
-                       strg[i][j] = 0; 
-                    }else{
+                } else {
+                    if (str.charAt(i) == str.charAt(j) && strg[i + 1][j - 1] == 0) {
+                        strg[i][j] = 0;
+                    } else {
                         int min = Integer.MAX_VALUE;
-                        for(int idash=i+1,jdash=j-gap;jdash<j;idash++,jdash++){
-                            min = Math.min(min,strg[i][jdash]+strg[idash][j]+1);
+                        for (int idash = i + 1, jdash = j - gap; jdash < j; idash++, jdash++) {
+                            min = Math.min(min, strg[i][jdash] + strg[idash][j] + 1);
                         }
                         strg[i][j] = min;
                     }
@@ -388,6 +394,61 @@ public class Intro {
             }
         }
 
-        return strg[0][strg.length-1];
+        return strg[0][strg.length - 1];
+    }
+
+    public static int KnapSack(int W, int[] wt, int[] val, int n,int[][] strg) {
+        if (W == 0 || n == 0) {
+            return 0;
+        }
+
+        if(strg[n][W]!=0){
+            return strg[n][W];
+        }
+
+        if (wt[n - 1] <= W) {
+            return strg[n][W] = Math.max(KnapSack(W, wt, val, n - 1,strg), KnapSack(W - wt[n - 1], wt, val, n - 1,strg) + val[n - 1]);
+        } else {
+            return strg[n][W] = KnapSack(W, wt, val, n - 1,strg);
+        }
+    }
+
+    public static boolean TargetSumProblem(int sum, int[] arr, int n,Boolean[][] strg) {
+        if(sum==0){
+            return true;
+        }
+
+        if(n==0){
+            return false;
+        }
+
+        if(strg[n][sum]!=null){
+            return strg[n][sum];
+        }
+
+        if (arr[n - 1] <= sum) {
+            return strg[n][sum] = (TargetSumProblem(sum, arr, n - 1,strg)||TargetSumProblem(sum - arr[n - 1], arr,n - 1,strg));
+        } else {
+            return strg[n][sum] = TargetSumProblem(sum, arr, n - 1,strg);
+        }
+    }
+
+    public static int KnapSackTabulation(int W, int[]wt, int n,int[] val){
+        int[][] strg = new int[n+1][W+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=W;j++){
+                if(i==0||j==0){
+                    strg[i][j] = 0;
+                }else{
+                    if(wt[i-1]<=j){
+                        strg[i][j] = Math.max(strg[i-1][j],val[i-1]+strg[i-1][j-wt[i-1]]);
+                    }else{
+                        strg[i][j] = strg[i-1][j];
+                    }
+                }
+            }
+        }
+
+        return strg[n][W];
     }
 }
